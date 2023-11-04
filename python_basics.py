b5326d351f6d14b2bd784f279b3e6016f3feda11
unknown <*vasumahi12@gmail.com*>
2023-11-04 12:41:29 +0530

    2/11/2023 class

---
+++ python_basics.py
@@ -0,0 +1,24 @@
+# Assignment operator
+a=10
+print(a)
+b=5
+print("the value of a:{}".format(a))
+a=20
+print("the value of a is after replacing is {}".format(a))
+# sum of a and b
+sum_a_b=a+b
+print("The sum of a and b is {}".format(sum_a_b))
+# difference of a and b
+diff_a_b=a-b
+print(" the difference of a and b is {}".format(diff_a_b))
+# product of a and b
+prod_a_b=a*b
+print(" the product of a and b is {}".format(prod_a_b))
+#to get quotient
+quot_a_b=a/b
+print(" the quotient when a divided by b is {}".format(quot_a_b))
+# to get reminder
+rem_a_b=a%b
+print("the reminder when a is divided by b is {}".format(rem_a_b))
+
+
